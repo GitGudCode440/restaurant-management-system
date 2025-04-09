7b49486087e6a92e6ce5f9056b89db1ea01a6a56
GitGudCode440 <m.mahadhashmi@gmail.com>
2025-04-09 16:46:15 +0500

    Add Orders and Billing functional UI

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <iostream>
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -43,3 +45,56 @@ void MainWindow::on_InventoryBtn_clicked()
     ui->stackedWidget->setCurrentIndex(5);
 }
 
+void MainWindow::addOrderCards(OrderCard* card) {
+    ui->OrderListLayout->addWidget(card);
+}
+
+void MainWindow::addOrder(OrderCard* card) {
+
+    QString orderId = QString("Order %1-%2")
+                          .arg(QDateTime::currentMSecsSinceEpoch())
+                          .arg(QUuid::createUuid().toString(QUuid::WithoutBraces));
+    card->changeOrderLabelText(orderId);
+    addOrderCards(card);
+}
+
+void MainWindow::on_FoodAddBtn_clicked()
+{
+    QComboBox* foodItemSelect = ui->FoodItemSelect;
+    QComboBox* foodQuantitySelect = ui->FoodQuantitySelect;
+
+    ui->FoodListWidget->addItem(
+        QString("%1 %2")
+            .arg(foodQuantitySelect->currentText(), foodItemSelect->currentText())
+    );
+}
+
+
+void MainWindow::on_FoodFinalizeBtn_clicked()
+{
+    QListWidget* listWidget = ui->FoodListWidget;
+    QComboBox* deliverySelect = ui->DeliveryTypeSelect;
+
+    OrderCard* card = new OrderCard;
+    OrderType type;
+
+    if (deliverySelect->currentIndex() >= 0 && deliverySelect->currentIndex() <= 2) {
+        type = static_cast<OrderType>(deliverySelect->currentIndex());
+    }
+    else type = OrderType::INVALID;
+
+    card->changeOrderType(type);
+
+    int count = listWidget->count();
+    for (int i = 0; i < count; ++i) {
+        QListWidgetItem *item = listWidget->item(i);
+        if (item) {
+            card->addTextToListWidget(item->text());
+        }
+    }
+
+    addOrder(card);
+
+    listWidget->clear();
+
+}
--- mainwindow.h
+++ mainwindow.h
@@ -2,12 +2,18 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QVector>
+#include <QUuid>
+#include <QDateTime>
+#include "ordercard.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
 class MainWindow;
 }
 QT_END_NAMESPACE
+
+class OrderCard;
 
 class MainWindow : public QMainWindow
 {
@@ -16,18 +22,20 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
-
 private slots:
     void on_TablesBtn_clicked();
-     void on_MenuBtn_clicked();
-      void on_OrdersBtn_clicked();
-       void on_BillingBtn_clicked();
-        void on_ReservationsBtn_clicked();
-         void on_InventoryBtn_clicked();
+    void on_MenuBtn_clicked();
+    void on_OrdersBtn_clicked();
+    void on_BillingBtn_clicked();
+    void on_ReservationsBtn_clicked();
+    void on_InventoryBtn_clicked();
+    void on_FoodAddBtn_clicked();
+    void on_FoodFinalizeBtn_clicked();
 
-
-
-    private:
+private:
     Ui::MainWindow *ui;
+    void addOrderCards(OrderCard* card);
+    void addOrder(OrderCard* card);
+
 };
 #endif // MAINWINDOW_H
